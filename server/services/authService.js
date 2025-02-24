e86fee21224a1f59b738701a684b17b6d3af76e7
kimminhyuck <rhkddka02@naver.com>
2025-02-24 16:34:41 +0900

    로그인 사용자 쿠키의 maxAge를 15분에서 7일로 변경

--- server/services/authService.js
+++ server/services/authService.js
@@ -192,7 +192,7 @@ exports.loginUser = async ({userid, password}, res) => {
     secure: isProduction,
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 크로스 사이트 쿠키 허용
     path: '/',
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 15분
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 7일
   });
 
   // 리프레시 토큰 쿠키 저장
