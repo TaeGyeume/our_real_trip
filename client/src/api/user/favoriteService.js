0719bb30aee3c0c5a8aee11cfd9bfd4e292b1c1e
TaeGyeume <crow0224@gmail.com>
2025-02-28 19:00:17 +0900

    QnA 및 즐겨찾기 API 경로를 환경변수 대신 하드코딩된 경로로 변경

--- client/src/api/user/favoriteService.js
+++ client/src/api/user/favoriteService.js
@@ -1,8 +1,8 @@
 import api from '../axios'; // axios.js에서 공통 설정을 가져옴
 
 // 서버 URL을 환경 변수로 설정 (로컬 환경에서만 사용, 배포 시 환경 변수로 변경 가능)
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api/favorites';
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api/favorites';
 
 //  itemType을 일관되게 소문자로 변환하는 함수
 const normalizeItemType = itemType => itemType.toLowerCase();
@@ -14,7 +14,8 @@ export const toggleFavorite = async (itemId, itemType) => {
     // console.log(` Sending request - itemId: ${itemId}, itemType: ${formattedItemType}`);
 
     const response = await api.post(
-      `${API_BASE_URL}/toggle`,
+      `/favorites/toggle`,
+      // ${API_BASE_URL}
       {itemId, itemType: formattedItemType},
       {withCredentials: true}
     );
@@ -32,7 +33,7 @@ export const toggleFavorite = async (itemId, itemType) => {
 //  사용자 즐겨찾기 목록 조회
 export const getUserFavorites = async () => {
   try {
-    const response = await api.get(API_BASE_URL, {withCredentials: true});
+    const response = await api.get('/favorites', {withCredentials: true});
     // console.log('Fetched favorites:', response.data.favorites);
     return response.data;
   } catch (error) {
