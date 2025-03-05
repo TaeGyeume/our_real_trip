04528d32b7d92c2a3f12413145172d15105ad39c
kimminhyuck <rhkddka02@naver.com>
2025-03-05 16:45:50 +0900

    TypeScript 버전 4.9.5로 다운그레이드 및 Naver, Kakao 아이콘 추가, 로그인 카드에 Naver 로그인 버튼 추가

--- client/src/pages/auth/style/theme/ColorModeSelect.tsx
+++ client/src/pages/auth/style/theme/ColorModeSelect.tsx
@@ -1,28 +1,18 @@
 import * as React from 'react';
-import { useColorScheme } from '@mui/material/styles';
+import {useColorScheme} from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectProps } from '@mui/material/Select';
+import Select, {SelectProps} from '@mui/material/Select';
 
 export default function ColorModeSelect(props: SelectProps) {
-  const { mode, setMode } = useColorScheme();
-  if (!mode) {
-    return null;
-  }
+  const {setMode} = useColorScheme(); // mode는 이제 필요없음
   return (
     <Select
-      value={mode}
-      onChange={(event) =>
-        setMode(event.target.value as 'system' | 'light' | 'dark')
+      value="light" // 항상 라이트 모드만 선택 가능
+      onChange={
+        event => setMode(event.target.value as 'light') // 'light'만 설정
       }
-      SelectDisplayProps={{
-        // @ts-ignore
-        'data-screenshot': 'toggle-mode',
-      }}
-      {...props}
-    >
-      <MenuItem value="system">System</MenuItem>
+      {...props}>
       <MenuItem value="light">Light</MenuItem>
-      <MenuItem value="dark">Dark</MenuItem>
     </Select>
   );
 }
