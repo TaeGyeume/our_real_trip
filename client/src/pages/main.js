ba83d81034fb3d3fb3f522ab295cececb87f684d
hihihi48 <glmwls9012@gmail.com>
2025-03-07 18:10:33 +0900

    UserList 컴포넌트를 main.js에서 주석 해제

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import {Box} from '@mui/material';
-// import UserList from '../components/tourTicket/UserList';
+import UserList from '../components/tourTicket/UserList';
 import PopularProductsSlider from '../components/views/PopularProductsSlider';
 import ConsoleLogo from '../components/common/ConsoleLogo';
 import 'bootstrap/dist/css/bootstrap.min.css';
