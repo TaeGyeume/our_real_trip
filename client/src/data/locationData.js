47a93211222b20b4adee35daec4194e167696166
TaeGyeume <crow0224@gmail.com>
2025-03-07 18:16:15 +0900

    커커밋밋

--- client/src/data/locationData.js
+++ client/src/data/locationData.js
@@ -2,48 +2,56 @@ export const locationData = [
   {
     id: 'seoul',
     title: '서울',
+    country: '대한민국',
     image: '/images/locationcard/seoul.jpg',
     description: '도시의 화려함을 즐겨보세요!'
   },
   {
     id: 'tokyo',
     title: '도쿄',
+    country: '일본',
     image: '/images/locationcard/tokyo2.jpg',
     description: '휴양지의 여유를 느껴보세요!'
   },
   {
     id: 'newyork',
     title: '뉴욕',
+    country: '미국',
     image: '/images/locationcard/newyork.jpg',
     description: '휴양지의 여유를 느껴보세요!'
   },
   {
     id: 'paris',
     title: '파리',
+    country: '프랑스',
     image: '/images/locationcard/paris.jpg',
     description: '빅벤과 함께 영국의 역사와 문화를'
   },
   {
     id: 'jeju',
-    title: '제주',
+    title: '제주도',
+    country: '대한민국',
     image: '/images/locationcard/jeju.jpg',
     description: '휴양지의 여유를 느껴보세요!'
   },
   {
     id: 'london',
     title: '런던',
+    country: '영국',
     image: '/images/locationcard/london.jpg',
     description: '바다와 산이 함께 있는 힐링 여행지'
   },
   {
     id: 'bangkok',
     title: '방콕',
+    country: '태국',
     image: '/images/locationcard/bangkok.jpg',
     description: '휴양지의 여유를 느껴보세요!'
   },
   {
     id: 'beijing',
     title: '베이징',
+    country: '중국',
     image: '/images/locationcard/beijing.jpg',
     description: '휴양지의 여유를 느껴보세요!'
   }
